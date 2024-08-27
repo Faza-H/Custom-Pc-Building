4d2b8ff4b51e15c5215fb53882a901ca3bba2d7c
M-Bilad <bilad900@gmail.com>
2024-08-27 20:14:55 +0500

    prebuild

--- frontend/pre-builds.js
+++ frontend/pre-builds.js
@@ -10,28 +10,3 @@ function toggleComments() {
         commentsSection.style.display = 'block';
     }
 }
-// Star Rating Functionality
-const stars = document.querySelectorAll('.star-rating .star');
-
-stars.forEach((star, index) => {
-    star.addEventListener('click', function() {
-        // Remove 'selected' class from all stars, then add it to all stars up to the clicked one
-        stars.forEach(s => s.classList.remove('selected'));
-        for (let i = stars.length; i =>; i--) {
-            stars[i].classList.add('selected');
-        }
-    });
-
-    star.addEventListener('mouseover', function() {
-        // Remove 'hover' class from all stars, then add it to all stars up to the hovered one
-        stars.forEach(s => s.classList.remove('hover'));
-        for (let i = stars.length; i =>; i--) {
-            stars[i].classList.add('hover');
-        }
-    });
-
-    star.addEventListener('mouseout', function() {
-        // Remove 'hover' class when the mouse leaves the stars
-        stars.forEach(s => s.classList.remove('hover'));
-    });
-});
