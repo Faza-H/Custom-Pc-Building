660ef26ccd99b634f8e06c4d275e037b80dbd9af
Fazi811 <sduper311@gmail.com>
2024-08-30 09:52:29 +0500

    recent

--- components.php
+++ components.php
@@ -1,4 +1,10 @@
 
+<?php 
+
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,24 +16,6 @@
 </head>
 <body>
     <header>
-        <h1>Custom PC Builder</h1>
-        <nav class="navbar">
-            <ul>
-                <li><a href="index.html">Home</a></li>
-            <li><a href="builder_1.html">PC Builder </a></li>
-            <li><a href="pre-builds.html">Pre Builds</a></li>
-            <li><a href="laptops.html">Laptops</a></li>
-            <!--Pre builds section would add the budget friendly and filtering option-->
-            <li><a href="components.html">Components</a></li>
-            <!-- handheld devices would include mobile devices and tablets-->
-            <li><a href="#">About Us</a></li>
-            <!--would include the goals, and about the company-->
-        <!--    <li><a href="#">Community</a></li>-->
-            <!-- community section would hold trends, future updates, user reviews and Expert Guidance recommandations-->
-            <li><a href="Sign In.html">Sign In</a></li>
-            <li><a href="Sign Up.html">Signup</a></li>
-        <!--    <li><a href="#">Support</a></li>-->
-            </ul>
         
     <div class="search">
       <input type="text" name="" id="find" placeholder="search here...." onkeyup="search()">
@@ -764,5 +752,8 @@
           </div>
         </div>
       </div>
+<?php 
+include('includes/footer.php');
+?>
     </body>
 </html>
