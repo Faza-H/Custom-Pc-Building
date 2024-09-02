c5f392a094d095ce29e92a02a7e7cfdb1d352474
Fazi811 <sduper311@gmail.com>
2024-09-02 20:50:27 +0500

    recent

--- admin/index.php
+++ admin/index.php
@@ -12,52 +12,7 @@ include('includes/navbar-top.php');
         <li class="breadcrumb-item active">Dashboard</li>
     </ol>
     <div class="row">
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-primary text-white mb-4">
-                <div class="card-body">
-                    Total categories
-                    <?php
-                        $dash_category_query = "SELECT * FROM categories";
-                        $dash_category_query_run = mysqli_query($con, $dash_category_query);
-                        if($category_total = mysqli_num_rows($dash_category_query_run))
-                        {
-                            echo '<h4 class="mb-0"> '.$category_total.' </h4>';
-                        }
-                        else
-                        {
-                            echo '<h4 class="mb-0">No Data</h4>';
-                        }
-                    ?>
-                    
-                </div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Total Products
-                    <?php
-                        $dash_product_query = "SELECT * FROM posts";
-                        $dash_product_query_run = mysqli_query($con, $dash_product_query);
-                        if($product_total = mysqli_num_rows($dash_product_query_run))
-                        {
-                            echo '<h4 class="mb-0"> '.$product_total.' </h4>';
-                        }
-                        else
-                        {
-                            echo '<h4 class="mb-0">No Data</h4>';
-                        }
-                    ?>
-                </div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-        </div>
+
         <div class="col-xl-3 col-md-6">
             <div class="card bg-success text-white mb-4">
                 <div class="card-body">Total Users
