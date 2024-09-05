dcce3fbef559b77a4f80be419f8aa14a2f97c644
Faza-H <rockeyrajput107@gmail.com>
2024-09-05 22:30:24 +0500

    pre builds backend done

--- pre-builds.js
+++
@@ -1,8 +0,0 @@
-function toggleComments() {
-    const commentsSection = document.querySelector('.comments-section');
-    if (commentsSection.style.display === 'block') {
-        commentsSection.style.display = 'none';
-    } else {
-        commentsSection.style.display = 'block';
-    }
-}
